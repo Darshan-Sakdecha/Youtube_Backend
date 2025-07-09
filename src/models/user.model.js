94a1f3ce1d1e909752a6ea08de10b2f66d091202
darshansakdecha <darshansakdecha@gmail.com>
2025-07-09 22:21:12 +0530

    add register controller

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -56,10 +56,9 @@ const userSchema = new Schema(
 //Before save your password here bcrypt.hash() is convert your password in encrypt format.
 //here 10 is salt means rounds.
 userSchema.pre("save", async function (next) {
-  
   if (!this.isModified("password")) return next();
 
-  this.password = bcrypt.hash(this.password, 10);
+  this.password = await bcrypt.hash(this.password, 10);
   next();
 });
 
