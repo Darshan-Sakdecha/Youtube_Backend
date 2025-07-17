c2d1e24270108641d5af30b5655e6b44ff3e9915
darshansakdecha <darshansakdecha@gmail.com>
2025-07-17 19:47:01 +0530

    Add Login & Logout

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,7 +3,7 @@ import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/Cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
-import { response } from "express";
+
 
 const generateAccessAndRefreshTokens = async (userId) => {
   try {
